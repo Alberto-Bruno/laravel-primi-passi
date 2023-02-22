562144b8b6112065698b3928e12d28a91d5db12e
Alberto-Bruno <brunoalberto97@outlook.it>
2023-02-22 18:04:02 +0100

    Bonus

--- resources/views/page2.blade.php
+++ resources/views/page2.blade.php
@@ -9,17 +9,17 @@
     <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/css/bootstrap.min.css'
         integrity='sha512-SbiR/eusphKoMVVXysTKG/7VseWii+Y3FdHrt0EpKgpToZeemhqHeZeLWLhJutz/2ut2Vw1uQEj2MbRF+TVBUA=='
         crossorigin='anonymous' />
-    <title>Hello Laravel</title>
+    <title>Page 2</title>
 </head>
 
 <body>
     <div class="container text-center h-25 py-5">
-        <h1>Hello Word</h1>
+        <h1>Laravel</h1>
     </div>
 
     <header class="d-flex justify-content-center align-items-center">
-        <a class="btn btn-danger me-3" href="{{ route('page1') }}"> Page 1</a>
-        <a class="btn btn-danger me-3" href="{{ route('page2') }}"> Page 2</a>
+        <a class="btn btn-danger me-3" href="{{ route('page1') }}">Return page1</a>
+        <a class="btn btn-danger me-3" href="{{ route('home') }}"> Home</a>
     </header>
 </body>
 
--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
-});
+})->name("home");
+
+Route::get('/page1', function () {
+    return view('page1');
+})->name('page1');
+
+Route::get('/page2', function () {
+    return view('page2');
+})->name('page2');
